43e07e37ab2b94c41ef9f4b5364f1cef42c17717
Adrián <adrimaqueda@gmail.com>
2025-02-15 16:57:43 +0100

    change calc with new js date proposal

--- src/lib/webBlocks/proxEvento.svelte
+++ src/lib/webBlocks/proxEvento.svelte
@@ -1,97 +1,103 @@
 <script>
-	import { onMount } from "svelte";
-    import { timeDay, timeMonth } from 'd3-time'
+	import { onMount } from 'svelte';
+	import { Temporal } from 'temporal-polyfill';
 
-    let { date, link } = $props();
+	let { date, link } = $props();
 
-    let fecha = $state('')
-    let timeToEvent = $state(undefined)
+	let fecha = $state('');
+	let timeToEvent = $state(undefined);
 
-    function interval(then, now) {
-        if (then <= now) throw new Error("Time’s up!");
-        let t = now;
-        let months = timeMonth.count(t, then);
-        if ((t = timeMonth.offset(t, months)) > then) t = timeMonth.offset(t, -1), --months;
-        let days = timeDay.count(t, then);
-        if ((t = timeDay.offset(t, days)) > then) t = timeDay.offset(t, -1), --days;
-        return { months, days };
-    }
+	function intervalString(interval) {
+		console.log(interval.days, interval.hours, interval.minutes);
+		if (interval.days <= -1) {
+			return undefined;
+		} else if (interval.days === 0) {
+			return `${interval.hours} horas`;
+		} else return `${interval.days} días`;
+	}
 
-    onMount(async () => {
-        let today = new Date()
+	onMount(async () => {
+		let hoy = Temporal.Now.zonedDateTimeISO();
+		fecha = Temporal.ZonedDateTime.from(date + 'T18:00:00Z[Europe/Madrid]');
 
-        fecha = new Intl.DateTimeFormat('es-ES', {dateStyle: 'full'}).format(new Date(date))
-        timeToEvent = interval(new Date(date), today)
-    })
-    
+		console.log(hoy.toString(), fecha.toString());
+
+		let interval = fecha.since(hoy, { largestUnit: 'day' });
+
+		timeToEvent = intervalString(interval);
+	});
 </script>
 
-<div  class="date-container">
-    <b>🗓️ Próximo evento:</b>
-    {#if timeToEvent?.days > 0}
-    <div style="text-align: center;padding-top:2rem">
-        <p class="date">{fecha}</p>
-        <p class="countdown">¡Nos vemos en {timeToEvent.days} días!</p>
-        <a href={link} class="tickets">Consigue tus entradas aquí 🎟️</a>
-    </div>
-    {:else}
-    <div class="tbd">
-        <p>Aún estamos decidiendo la fecha!</p>
-        <p>Entérete en <a href="https://www.datavisualizationsociety.org/slack-community">Slack</a> <span style="display: inline-block;">(#location-madrid)</span></p>
-    </div>
-    {/if}
+<div class="date-container">
+	<b>🗓️ Próximo evento:</b>
+	{#if timeToEvent !== undefined}
+		<div style="text-align: center;padding-top:2rem">
+			<p class="date">{fecha.toLocaleString('es-ES', { dateStyle: 'full', timeStyle: 'short' })}</p>
+			<p class="countdown">¡Nos vemos en {timeToEvent}!</p>
+			<a href={link} class="tickets">Consigue tus entradas aquí 🎟️</a>
+		</div>
+	{:else}
+		<div class="tbd">
+			<p>Aún estamos decidiendo la fecha!</p>
+			<p>
+				Entérete en <a href="https://www.datavisualizationsociety.org/slack-community">Slack</a>
+				<span style="display: inline-block;">(#location-madrid)</span>
+			</p>
+		</div>
+	{/if}
 </div>
 
 <style>
-    .date-container {
-        background-color: #f0f0f0; 
-        padding: 2rem;
-    }
+	.date-container {
+		background-color: #f0f0f0;
+		padding: 2rem;
+	}
 
-    .date, .countdown {
-        margin-bottom: 3rem;
-    }
+	.date,
+	.countdown {
+		margin-bottom: 3rem;
+	}
 
-    .date {
-        font-weight: bold;
-        font-size: 1.1rem
-    }
+	.date {
+		font-weight: bold;
+		font-size: 1.1rem;
+	}
 
-    .countdown {
-        font-size: 1.5rem
-    }
+	.countdown {
+		font-size: 1.5rem;
+	}
 
-    .tickets {
-        font-size: 1rem
-    }
+	.tickets {
+		font-size: 1rem;
+	}
 
-    .tbd {
-        text-align: center;
-        padding: 2rem 0;
-        font-size: 1.2rem
-    }
+	.tbd {
+		text-align: center;
+		padding: 2rem 0;
+		font-size: 1.2rem;
+	}
 
-    .tbd a {
-        text-decoration: underline;
-        font-weight: bold;
-        color: purple
-    }
+	.tbd a {
+		text-decoration: underline;
+		font-weight: bold;
+		color: purple;
+	}
 
-    @media (width > 550px) {
-        .date-container {
-            padding: 3rem
-        }
+	@media (width > 550px) {
+		.date-container {
+			padding: 3rem;
+		}
 
-        .date {
-            font-size: 1.25rem
-        }
+		.date {
+			font-size: 1.25rem;
+		}
 
-        .countdown {
-            font-size: 1.7rem
-        }
+		.countdown {
+			font-size: 1.7rem;
+		}
 
-        .tickets {
-            font-size: 1.2rem
-        } 
-    }
+		.tickets {
+			font-size: 1.2rem;
+		}
+	}
 </style>
